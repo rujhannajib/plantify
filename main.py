784fd5d97c046a744eecd602ac9db59fdfce956d
rujhannajib <janjibdev@gmail.com>
2026-01-02 21:42:28 -0500

    Finished the prediction part

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 from flask import Flask, render_template, request
 import os
+from transformers import pipeline
+from PIL import Image
+import json
 
 app =  Flask(__name__)
 app.config["UPLOAD_FOLDER"] = "static/uploads"
 
+id_2_species = "class_idx_to_species_id.json"
+species_2_name = "plantnet300K_species_id_2_name.json"
+
+with open(id_2_species, 'r') as file:
+    id_2_species_dict = json.load(file)
+
+with open(species_2_name, 'r') as file:
+    species_2_name_dict = json.load(file)
+
+def get_species_name(label:str):
+    return species_2_name_dict[id_2_species_dict[str(label)]]
+
+# Load your Hugging Face model
+classifier = pipeline(
+    "image-classification",
+    model="janjibDEV/vit-plantnet300k"
+)
+
 @app.route("/", methods=["GET","POST"])
 def index():
-
+    prediction = None
     display_image = None
 
     if request.method == "POST":    
@@ -15,7 +36,12 @@ def index():
         files.save(image_path)
         display_image = image_path
 
-    return render_template("index.html", display_image=display_image)
+        target_image = Image.open(display_image)
+        res = classifier(target_image)
+
+        prediction = get_species_name(res[0]["label"])
+        # print(prediction)
+    return render_template("index.html", display_image=display_image, prediction=prediction)
 
 def main():
     app.run(debug=True)
